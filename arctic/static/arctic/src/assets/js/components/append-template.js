cd59c210103e6d64180eafdde40ec6e3f3f8690a
R4YM3 <raymond@hetwebbureau.nl>
2016-11-10 13:41:44 +0100

    append template js

--- arctic/static/arctic/src/assets/js/components/append-template.js
+++ arctic/static/arctic/src/assets/js/components/append-template.js
@@ -1,58 +1,154 @@
+/*
+    appends a template to element on click
+    * when a template has form fields, then it updates the unique id and name (for labels for attr)
+    * safely clones selectize selectboxes
+
+    required arguments:
+    * data-template = CSS selector to template
+    * data-template-button = button to trigger append()
+
+    dependecies:
+    * helpers.js ( getInt )
+    * selectize
+ */
+
 ( function ( $ ) {
     'use strict'
 
-    function AppendTemplate ( element ) {
-        this.element = $( '[data-append-template]' )
-        this.template = this.element.data( 'append-template' )
-        this.button = '<a class="button">Add</a>'
 
-        // define template
-        if ( this.template ) {
-            this.template = this.element.find( this.template )
+    function appendTemplate ( element ) {
+        this.element = $( "[data-template][data-template-button]" );
+        this.templateClass = this.element.data( 'template' );
+        this.template = this.element.find( this.templateClass );
+        this.button = this.element.data( 'template-button' );
+        this.button = this.element.parent().find( this.button );
+
+        // when there's no template and button found in DOM
+        if ( !this.template.length && !this.button.length ) {
+            throw new Error( 'Cannot find template' );
         }
 
-        // init
-        if ( this.template != undefined && this.template.size() ) {
-            this.template = this.template.clone()
+        var self = this;
+        self.init();
+    };
 
-            var self = this
 
-            // window.self = self;
-            self.setup()
-            self.init()
-        }
-    }
+    appendTemplate.prototype.init = function ( ) {
+        var self = this;
 
-    AppendTemplate.prototype.setup = function ( ) {
-        var self = this
-
-        // disable selectize when in template
-        self.template.find("select").each(function(){
-            $(this)[0].selectize.destroy();
-
-            if ( this.selectize ) {
-                this.selectize.destroy();
-            }
-        });
-
-        // append add button
-        self.element.after( self.button )
-        self.button = self.element.next()
-    }
-
-    AppendTemplate.prototype.init = function ( ) {
-        var self = this
+        self.createTemplate();
 
         self.button.on( 'click', function () {
+            self.appendTemplate();
+        });
+    };
 
-            self.element.append( self.template.clone() )
-            selectize()
-            self.element.trigger( 'recalc' );
-        })
-    }
+
+    // create an template within self.template
+    appendTemplate.prototype.createTemplate = function ( ) {
+        var self = this;
+
+        // is selectize loaded?
+        if ( jQuery().selectize ) {
+
+            // if there's a selectize selectbox, destroy it before cloning
+            var select = self.template.find( 'select' );
+            var selectize = select[0].selectize.destroy();
+        }
+
+        // remove template indentifier
+        self.template.removeClass( self.templateClass.substring(1) ); // remove . in classname
+
+        // clone template
+        self.templateCache = self.template; // so we can delete it
+        self.template = self.template.clone();
+
+        // remove orignal template and cache object
+        self.templateCache.remove();
+        delete self.templateCache;
+    };
+
+
+    // appends a template
+    appendTemplate.prototype.appendTemplate = function ( ) {
+        var self = this;
+
+        // create an instance of template to append
+        var template = self.template.clone();
+
+        // update ids of appended fields
+        template = self.updateIDs( template );
+
+        // append template
+        self.element.append( template );
+
+        // re-initiate selectize
+        selectize();
+
+        // trigger sortable recalc
+        self.element.trigger( 'recalc' );
+    };
+
+
+    // updates id's of form fields and labels within argrumented templat
+    appendTemplate.prototype.updateIDs = function ( template ) {
+        var self = this
+
+        // get elements that has an unique id which need to be updated
+        var label = template.find( 'label' );
+        var input = template.find( 'input:not( [type="submit"] )' );
+        var select = template.find( 'select' );
+        var textarea = template.find( 'textarea' );
+
+        var fields = label.add( input ).add( select ).add( textarea );
+
+        if ( fields.length ) {
+
+            if ( !self.id ) {
+                self.id = {};
+                self.id.init = fields.last().attr('id');
+                self.id.init = getInt( self.id.init );
+            }
+
+            // set new current id
+            if ( self.id.current ) {
+                self.id.current++;
+            } else {
+                self.id.current = self.id.init + 1;
+            }
+
+            // update id's in template
+            for ( var i = 0; i < fields.length; i++ ) {
+                var element = fields[i];
+
+                if ( element.nodeName == 'LABEL' ) {
+                    var target = element.htmlFor;
+                    element.htmlFor = target.replace( self.id.init, self.id.current );
+                } else {
+                    var name = element.name;
+                    element.name = name.replace( self.id.init, self.id.current );
+
+                    var id = element.id;
+                    element.id = id.replace( self.id.init, self.id.current );
+                }
+            }
+        };
+
+        return template;
+    };
+
 
     $(document).ready(function() {
-        new AppendTemplate()
-    })
+        new appendTemplate();
+    });
 
-} )( jQuery )
+
+} )( jQuery );
+
+/*
+    TODO
+    selectize.js
+
+    selectize.destroy();
+    selectize.reinit();
+*/
